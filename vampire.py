bf2a7cda5fe413b730e9b708a1bd246ab98bcc00
lukeseeto <llseeto@outlook.com>
2024-08-02 17:16:13 +1000

    Made code easier to read and added global constants.

--- vampire.py
+++ vampire.py
@@ -1,4 +1,5 @@
 import os
+from consts import *
 
 class Room:
     def __init__(self, id, adj, pos, name):
@@ -10,110 +11,108 @@ class Room:
         self.pos = pos
 
 class Player:
-    def __init__(self):
-        self.health = 2
+    def __init__(self, id):
+        self.id = id
+        self.health = PLAYER_HEALTH
         self.room = None
-        self.light = 5
-        self.stakes = 5
+        self.light = PLAYER_LIGHT
+        self.stakes = PLAYER_STAKES
         self.damaged = False
         self.recover = False
 
     def damage(self):
-        self.health -= 1
+        self.health -= DRACULA_DAMAGE
         self.damaged = True
 
     def move(self, room):
         # Can player stay in same room / move 0?
-        if room.id in self.room.adj:
-            self.room = room
-            return True
-        else:
+        if not room.id in self.room.adj:
             print("That room is not adjacent.")
             return False
+           
+        self.room = room
+        return True
        
     def garlic(self, room, dracula):
-        if room.id in self.room.adj:
-            if room.id == dracula.room.id:
-                print("Dracula is in that room.")
-            else:
-                print("Dracula is not in that room.")
-            return True
-        else:
+        if not room.id in self.room.adj:
             print("That room is not adjacent.")
             return False
+            
+        if room.id == dracula.room.id:
+            print("Dracula is in that room.")
+        else:
+            print("Dracula is not in that room.")
+        return True
         
     def throw_stake(self, room, dracula):
-        if room.id in self.room.adj:
-            if self.stakes > 0:
-                self.stakes -= 1
-                if room.id == dracula.room.id:
-                    dracula.damage()
-                print("You threw a stake.")
-                return True
-            else:
-                print("You have no more stakes.")
-                return False
-        else:
+        if not room.id in self.room.adj:
             print("That room is not adjacent.")
+            return False
+        if self.stakes <= 0:
+            print("You have no more stakes.")
             return False
         
+        self.stakes -= 1
+        if room.id == dracula.room.id:
+            dracula.damage()
+        print("You threw a stake.")
+        return True
+    
+        
     def make_light(self, room):
-        if room.id in self.room.adj:
-            if self.light > 0:
-                self.light -= 1
-                room.light = True
-                print("You created light.")
-                return True
-            else:
-                print("You can't make anymore light.")
-                return False
-        else:
+        if not room.id in self.room.adj:
             print("That room is not adjacent.")
             return False
-
+        if self.light <= 0:
+            print("You can't make anymore light.")
+            return False
+        
+        self.light -= 1
+        room.light = True
+        print("You created light.")
+        return True
+    
 class Dracula:
     def __init__(self):
-        self.health = 3
+        self.health = DRACULA_HEALTH
         self.room = None
     
     def damage(self):
-        self.health -= 1
+        self.health -= PLAYER_DAMAGE
     
     def move(self, room, players):
-        if room.id in self.room.adj:
-            if not room.light:
-                self.room = room
-                for player in players:
-                    if player.room.id == room.id and not player.damaged and not player.recover:
-                        player.damage()
-                return True
-            else:
-                print("That room contains light.")
-                return False
-        else:
+        if not room.id in self.room.adj:
             print("That room is not adjacent.")
             return False
+        if room.light:
+            print("That room contains light.")
+            return False
+        
+        self.room = room
+        for player in players:
+            if player.room.id == room.id and not player.damaged and not player.recover:
+                player.damage()
+        return True
 
 class Gamestate:
     def __init__(self, players, layout):
-        if players <= 4:
-            self.rooms = []
-            self.dracula = Dracula()
-            self.players = [Player() for _ in range(players)]
+        self.rooms = []
+        self.dracula = Dracula()
+        self.players = [Player(i + 1) for i in range(players)]
 
-            if layout == 0:
-                self.rooms.append(Room(1, [1, 2, 4], (0, 0), "r1"))
-                self.rooms.append(Room(2, [2, 1, 3], (1, 0), "r2"))
-                self.rooms.append(Room(3, [3, 2, 6], (2, 0), "r3"))
-                self.rooms.append(Room(4, [4, 1, 7], (0, 1), "r4"))
-                self.rooms.append(Room(6, [6, 3, 9], (2, 1), "r6"))
-                self.rooms.append(Room(7, [7, 4, 8], (0, 2), "r7"))
-                self.rooms.append(Room(8, [8, 7, 9], (1, 2), "r8"))
-                self.rooms.append(Room(9, [9, 6, 8], (2, 2), "r9"))
+        if layout == 0:
+            self.rooms.append(Room(1, [1, 2, 4], (0, 0), "r1"))
+            self.rooms.append(Room(2, [2, 1, 3], (1, 0), "r2"))
+            self.rooms.append(Room(3, [3, 2, 6], (2, 0), "r3"))
+            self.rooms.append(Room(4, [4, 1, 7], (0, 1), "r4"))
+            self.rooms.append(Room(6, [6, 3, 9], (2, 1), "r6"))
+            self.rooms.append(Room(7, [7, 4, 8], (0, 2), "r7"))
+            self.rooms.append(Room(8, [8, 7, 9], (1, 2), "r8"))
+            self.rooms.append(Room(9, [9, 6, 8], (2, 2), "r9"))
 
-                for player in self.players:
-                    player.room = self.find_room("r1")
-                self.dracula.room = self.find_room("r9")
+            for player in self.players:
+                player.room = self.find_room("r1")
+            self.dracula.room = self.find_room("r9")
 
     def find_room(self, name):
         matches = [x for x in self.rooms if x.name == name]
@@ -126,38 +125,38 @@ class Gamestate:
         for room in self.rooms:
             w = max(room.pos[0], w)
             h = max(room.pos[1], h)
-        w = (w + 1) * 6
-        h = (h + 1) * 4
-        grid = [[" "] * w for _ in range(h)]
+        w = (w + 1) * ROOM_WIDTH
+        h = (h + 1) * ROOM_HEIGHT
+        grid = [[EMPTY_CHAR] * w for _ in range(h)]
 
         for room in self.rooms:
             x, y = room.pos 
-            x *= 6
-            y *= 4
-            for i in range(6):
-                grid[y][x + i] = "#"
-                grid[y + 3][x + i] = "#"
-            for i in range(2):
-                grid[y + 1 + i][x] = "#"
-                grid[y + 1 + i][x + 5] = "#"
+            x *= ROOM_WIDTH
+            y *= ROOM_HEIGHT
+
+            for i in range(ROOM_WIDTH):
+                grid[y][x + i] = WALL_CHAR
+                grid[y + ROOM_HEIGHT - 1][x + i] = WALL_CHAR
+            for i in range(ROOM_HEIGHT - 2):
+                grid[y + 1 + i][x] = WALL_CHAR
+                grid[y + 1 + i][x + ROOM_WIDTH - 1] = WALL_CHAR
             if room.light:
-                grid[y + 2][x + 3] = "L"
+                grid[y + 2][x + 3] = LIGHT_CHAR
             grid[y][x + 2] = room.name[0]
             grid[y][x + 3] = room.name[1]
         
-        player_pos = {0:(0, 0), 1:(1, 0), 2:(0, 1), 3:(1, 1)}
         for i in range(len(self.players)):
             x, y = self.players[i].room.pos 
-            x *= 6
-            y *= 4
-            d_x, d_y = player_pos[i]
-            grid[y + 1 + d_y][x + 1 + d_x] = str(i + 1)
+            x *= ROOM_WIDTH
+            y *= ROOM_HEIGHT
+            d_x, d_y = PLAYER_OFFSET[i]
+            grid[y + 1 + d_y][x + 1 + d_x] = str(self.players[i].id)
         
         if show_dracula:
             x, y = self.dracula.room.pos 
-            x *= 6
-            y *= 4
-            grid[y + 1][x + 3] = "D"
+            x *= ROOM_WIDTH
+            y *= ROOM_HEIGHT
+            grid[y + 1][x + 3] = DRACULA_CHAR
 
         print("\n".join(["".join(line) for line in grid]) + "\n")
     
@@ -167,7 +166,7 @@ class Gamestate:
         light = ["Light"]
         stakes = ["Stakes"]
         for i in range(len(self.players)):
-            names.append(f"P{i + 1}")
+            names.append(f"P{self.players[i].id}")
             health.append(str(self.players[i].health))
             light.append(str(self.players[i].light))
             stakes.append(str(self.players[i].stakes))
@@ -187,10 +186,10 @@ class Gamestate:
 
         for i in range(len(self.players) - 1, -1, -1):
             if self.players[i].health == 0:
-                print(f"Player {i + 1} has died.")
+                print(f"Player {self.players[i].id} has died.")
                 self.players.pop(i)
             elif self.players[i].damaged:
-                print(f"Player {i + 1} was bitten.")
+                print(f"Player {self.players[i].id} was bitten.")
         
         for i in range(len(self.players)):
             if self.players[i].damaged:
@@ -200,14 +199,14 @@ class Gamestate:
             if self.players[i].recover:
                 self.players[i].recover = False
 
-            self.draw_map(False)
-            self.draw_stats(False)
+            self.draw_map(PLAYER_GODMODE)
+            self.draw_stats(PLAYER_GODMODE)
 
             moved = False
             garlic = False
 
             while True:
-                command = input(f"Player {i + 1}: ").split(" ", 1)
+                command = input(f"Player {self.players[i].id}: ").split(" ", 1)
                 if len(command) == 1:
                     command += ['']
                 action, arg = command
@@ -221,9 +220,9 @@ class Gamestate:
                     print("stake  [room] Throw a stake into an adjacent room to attack Dracula.")
                     print("skip          End your turn.")
                 elif action == "map":
-                    self.draw_map(False)
+                    self.draw_map(PLAYER_GODMODE)
                 elif action == "stats":
-                    self.draw_stats(False)
+                    self.draw_stats(PLAYER_GODMODE)
                 elif action == "mv":
                     if moved:
                         print("You have already moved on your turn.")
@@ -296,7 +295,7 @@ class Gamestate:
                 moved = self.dracula.move(room, self.players)
                 if moved:
                     moves += 1
-                    if moves == 3:
+                    if moves == DRACULA_MOVES:
                          break
             elif action == "skip":
                 break
@@ -308,7 +307,7 @@ class Gamestate:
         return False
 
 def main():
-    gamestate = Gamestate(int(input("How many players? ")), 0)
+    gamestate = Gamestate(PLAYER_COUNT, LAYOUT)
     while True:
         if gamestate.player_turn():
             print("Dracula has been defeated. Players win!")
@@ -316,6 +315,8 @@ def main():
         if gamestate.dracula_turn():
             print("All players have been killed. Dracula wins!")
             break
+    while True:
+        pass
 
 if __name__ == "__main__":
     main()
