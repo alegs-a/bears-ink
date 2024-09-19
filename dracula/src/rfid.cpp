5d157903b760b8c351e0d5b73084a220fce3537a
Max Guppy <theonly@mrcat.au>
2024-09-19 15:07:45 +1000

    Improve logic and documentation in rfid code

--- dracula/src/rfid.cpp
+++ dracula/src/rfid.cpp
@@ -18,27 +18,36 @@ static K_THREAD_DEFINE(rfid, RFID_THREAD_STACK_SIZE,
 // Defined and initialised by the above macro.
 extern const k_tid_t rfid_thread_id;
 
-MFRC522 mfrc522(0x2c, 3 /* Reset pin */); // Create MFRC522 instance.
+MFRC522 mfrc522(0x2c, 3 /* Reset pin */);
 
 bool rfid_init()
 {
+    // We can't do our initialisation here because the `mfrc522` variable above
+    // isn't initialised by the C++ runtime until `rfid_main` is called.
     return true;
 }
 
 void detect_new_card()
 {
-    if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    if (!mfrc522.PICC_IsNewCardPresent()) {
         return;
     }
 
     // Read the new card's UID into mfrc522.uid
-    if ( ! mfrc522.PICC_ReadCardSerial()) {
+    if (!mfrc522.PICC_ReadCardSerial()) {
         return;
     }
 
-    // I don't want to deal with the full NDEF spec, so I'll just read a
-    // fixed address where the first record's URL happens to be.
-    // It's split in two because we're only reading 16 bytes at a time.
+    /*
+     * Bears Ink tokens contain absolute URLs to https://thebears.ink/game/kind
+     * as an identifier. They can be scanned e.g. a player's phone to take them
+     * to our website, and created by players at will with a standard NFC
+     * reader/writer app.
+     *
+     * Ideally, we should be parsing the NDEF metadata to extract the URL, but
+     * I couldn't figure out how it fits in the tag, so I'm parsing a fixed
+     * offset based on empirical evidence.
+     */
 
     // Read the metadata and the first half of the URL
     byte data[18];
@@ -47,8 +56,8 @@ void detect_new_card()
 
     byte url_len = data[1]; // Payload length field
 
-    const char *tld = "thebears.ink";
-    if (strncmp(tld, reinterpret_cast<char*>(data + 4), 12) != 0) {
+    // Make sure it's a Bears Ink token
+    if (strncmp(reinterpret_cast<char*>(data + 4), "thebears.ink", 12) != 0) {
         printk("Unrecognised token:\n");
         mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
         return;
@@ -59,36 +68,39 @@ void detect_new_card()
     mfrc522.MIFARE_Read(10, data, &data_len);
 
     // Null-terminate the payload so it can be interpreted as a string.
-    // 13 here represents "thebears.ink" and its leading 0x04 (https)
+    // 13 here represents "thebears.ink" and the leading protocol byte
     data[url_len - 13] = '\0';
+    char *game = reinterpret_cast<char*>(data);
 
+    // Make sure it's a Dracula game token
+    if (strncmp(game, "/dracula/", 9) != 0) {
+        printk("Unrecognised Bears Ink token: %s\n", game);
+        mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+        return;
+    }
+
+    // Check what kind of token it is
+    char *kind = game + 9;
     enum TokenKind token_kind;
-    const char *player1 = "/dracula/player1";
-    const char *player2 = "/dracula/player2";
-    const char *player3 = "/dracula/player3";
-    const char *player4 = "/dracula/player4";
-    const char *garlic = "/dracula/garlic";
-    const char *sunlight = "/dracula/sun";
-    const char *holy_water = "/dracula/water";
-    if (url_len == 29 && strcmp(player1, reinterpret_cast<char*>(data)) == 0) {
+    if (strcmp(kind, "player1") == 0) {
         token_kind = Player1;
         printk("Found Bears Ink token: Player1\n");
-    } else if (url_len == 29 && strcmp(player2, reinterpret_cast<char*>(data)) == 0) {
+    } else if (strcmp(kind, "player2") == 0) {
         token_kind = Player2;
         printk("Found Bears Ink token: Player2\n");
-    } else if (url_len == 29 && strcmp(player3, reinterpret_cast<char*>(data)) == 0) {
+    } else if (strcmp(kind, "player3") == 0) {
         token_kind = Player3;
         printk("Found Bears Ink token: Player3\n");
-    } else if (url_len == 29 && strcmp(player4, reinterpret_cast<char*>(data)) == 0) {
+    } else if (strcmp(kind, "player4") == 0) {
         token_kind = Player4;
         printk("Found Bears Ink token: Player4\n");
-    } else if (url_len == 28 && strcmp(garlic, reinterpret_cast<char*>(data)) == 0) {
+    } else if (strcmp(kind, "garlic") == 0) {
         token_kind = Garlic;
         printk("Found Bears Ink token: Garlic\n");
-    } else if (url_len == 25 && strcmp(sunlight, reinterpret_cast<char*>(data)) == 0) {
+    } else if (strcmp(kind, "sun") == 0) {
         token_kind = Sunlight;
         printk("Found Bears Ink token: Sunlight\n");
-    } else if (url_len == 27 && strcmp(holy_water, reinterpret_cast<char*>(data)) == 0) {
+    } else if (strcmp(kind, "water") == 0) {
         token_kind = HolyWater;
         printk("Found Bears Ink token: HolyWater\n");
     } else {
